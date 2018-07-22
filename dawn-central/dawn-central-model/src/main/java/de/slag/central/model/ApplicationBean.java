d721b560db5a130425126157199c5f7497a9d2c0
sebastian <sebastian@kallisto.fritz.box>
2018-07-23 00:35:37 +0200

    base structures of 'model'

--- dawn-central/dawn-central-model/src/main/java/de/slag/central/model/ApplicationBean.java
+++ dawn-central/dawn-central-model/src/main/java/de/slag/central/model/ApplicationBean.java
@@ -1,8 +1,14 @@
 package de.slag.central.model;
 
+import java.time.LocalDate;
+
 public interface ApplicationBean {
 
 	Long getId();
+
+	LocalDate getCreated();
+
+	boolean isDeleted();
 
 	/**
 	 * @return A human readable label of this bean.
