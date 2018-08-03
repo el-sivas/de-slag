ae9a94acaa325f18d60f65f1e3d008857b7f81df
sebastianl <sebastianl@LAPD5BHXZ1.subito.de>
2018-08-03 17:45:48 +0200

    -DatabaseDialect and Driver enum

--- dawn-central/dawn-central-data/src/main/java/de/slag/central/data/database/DatabaseDialect.java
+++
@@ -1,7 +0,0 @@
-package de.slag.central.data.database;
-
-public interface DatabaseDialect {
-	
-	String MARIA_DB = "org.hibernate.dialect.MariaDB53Dialect";
-
-}
--- dawn-central/dawn-central-data/src/main/java/de/slag/central/data/database/DatabaseDriverClass.java
+++
@@ -1,7 +0,0 @@
-package de.slag.central.data.database;
-
-public interface DatabaseDriverClass {
-	
-	String MARIA_DB = "org.mariadb.jdbc.Driver";
-
-}
